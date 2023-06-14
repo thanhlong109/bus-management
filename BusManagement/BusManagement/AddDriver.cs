cb29429d887967301926396e755670a51dd20935
Vo Ngoc Tan Dung <vongoctandung2002@gmail.com>
2023-06-14 15:31:24 +0700

    Update add method in AddDriver Form

--- BusManagement/BusManagement/AddDriver.cs
+++ BusManagement/BusManagement/AddDriver.cs
@@ -1,4 +1,5 @@
-﻿using Services.Repository;
+﻿using Services.Models;
+using Services.Repository;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
@@ -15,7 +17,7 @@ namespace BusManagement
     public partial class AddDriver : Form
     {
         BusRepository _busrepository = new BusRepository();
-
+        DriverRepository _driverrepository = new DriverRepository();
         public AddDriver()
         {
             InitializeComponent();
@@ -32,29 +34,34 @@ namespace BusManagement
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            String idPattern = @"^d[0-9]{4}|D[0-9]{4}$";
-            String id = txtDriverID.Text;
-            String name = txtDriverName.Text;
-            String address = txtDriverAddress.Text;
-            String salary = txtDriverSalary.Text;
-            if (!checkNullOrEmpty())
-            {
+            var driver = getValidInput();
 
+            if (driver != null)
+            {
+                _driverrepository.Create(driver);
+                MessageBox.Show("Thêm tài xế thành công!", "Thông báo", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+                Form form = new DriverManagement();
+                form.ShowDialog();
             }
             else
             {
-
+                MessageBox.Show("Vui lòng thử lại!", "Thông báo", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+
+
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void load_cbDriverGender()
         {
-            cbDriverGender.Items.AddRange(new String[] {"Nam","Nu","Khac" });
+            cbDriverGender.Items.AddRange(new String[] { "Nam", "Nu", "Khac" });
             cbDriverGender.SelectedIndex = 0;
         }
 
@@ -78,17 +85,112 @@ namespace BusManagement
             load_cbBusID();
         }
 
-        public bool checkNullOrEmpty()
+        private TblDriver getValidInput()
         {
-            bool check = true;
-            if (String.IsNullOrEmpty(txtDriverID.Text) 
-                && String.IsNullOrEmpty(txtDriverName.Text) 
-                && String.IsNullOrEmpty(txtDriverAddress.Text) 
-                && String.IsNullOrEmpty(txtDriverSalary.Text))
+            // New driver
+            TblDriver driver = new TblDriver();
+
+            // Check if textbox is null or empty?
+            if (String.IsNullOrEmpty(txtDriverID.Text.Trim())
+                || String.IsNullOrEmpty(txtDriverName.Text.Trim())
+                || String.IsNullOrEmpty(txtDriverAddress.Text.Trim())
+                || String.IsNullOrEmpty(txtDriverSalary.Text.Trim())
+                )
             {
-                check = false;
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin!", "Thông báo", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
             }
-            return check;
+
+            //Check if "ID" match regex or not?
+            String idPattern = @"^[d,D]{1}[0-9]{4}$";
+            if (!Regex.IsMatch(txtDriverID.Text.Trim(), idPattern))
+            {
+                MessageBox.Show("Vui lòng điền ID theo cú pháp sau: D****. " +
+                    "Trong đó * là số. Ví Dụ: D1234 ", "Thông báo", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Check if "ID" is existed or not?
+            var existedDriverID = _driverrepository.GetAll()
+                .Where(p => p.DriverId.Equals(txtDriverID.Text.Trim().ToUpper())).FirstOrDefault();
+            if (existedDriverID != null)
+            {
+                MessageBox.Show("ID đã tồn tại!!!", "Thông báo", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Add "ID" to new Driver
+            driver.DriverId = txtDriverID.Text.Trim().ToUpper();
+
+            //Check if "Name" contains special characters or numbers or not?
+            if (!txtDriverName.Text.Trim().All(c => Char.IsLetter(c) || c == ' '))
+            {
+                MessageBox.Show("Tên không chứa ký tự đặc biệt và số!!!", "Thông báo",
+               MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Add "Name" to new Driver
+            driver.DriverName = txtDriverName.Text.Trim();
+
+            //Check if "Address" contains special characters or not?
+            if (!txtDriverAddress.Text.Trim().All(c => Char.IsLetterOrDigit(c) || c == ' '))
+            {
+                MessageBox.Show("Địa chỉ không chứa ký tự đặc biệt!!!", "Thông báo",
+               MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Add "Address" to new Driver
+            driver.Address = txtDriverAddress.Text.Trim();
+
+            //Check if "StartDate" right or not?
+            if (dtpDriverStartDate.Value <= DateTime.Today)
+            {
+                MessageBox.Show("Ngày bắt đầu phải lớn hơn hoặc bằng ngày hôm nay!!!", "Thông báo",
+              MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Add "StartDate" to new Driver
+            driver.StartDate = dtpDriverStartDate.Value;
+
+            //Check if "DoB" right or not?
+            if (dtpDriverDoB.Value >= DateTime.Today)
+            {
+                MessageBox.Show("Ngày sinh không thể nào lớn hơn ngày hôm nay!!!", "Thông báo",
+              MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Add "DoB" to new Driver
+            driver.Dob = dtpDriverDoB.Value;
+
+            //Check if "Salary" contains special characters or alphabet or not?
+            if (!txtDriverSalary.Text.Trim().All(c => Char.IsDigit(c)))
+            {
+                MessageBox.Show("Lương không chứa ký tự đặc biệt và chữ cái!!!", "Thông báo",
+             MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return null;
+            }
+
+            //Add "Salary" to new Driver
+            driver.Salary = Decimal.Parse(txtDriverSalary.Text.Trim());
+
+            //Add "Gender" to new Driver
+            driver.Gender = cbDriverGender.SelectedItem.ToString();
+
+            //Add "BusID" to new Driver
+            driver.BusId = cbBusID.SelectedValue.ToString();
+
+            //Add "isActive" to new Driver
+            driver.IsActive = true;
+
+            return driver;
         }
+
     }
 }
